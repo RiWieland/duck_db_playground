f309a6cb4157f88f63fa435b884aa582b2e452dc
RiWieland <rich.wieland73@gmail.com>
2024-04-16 00:33:28 +0200

    add struct for config crawl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use serde_yaml;
+
+use serde_yaml::Value;
+use serde::de::Error;
+
+
+#[derive(serde::Deserialize)]
+struct configCrawl {
+    CompanyName : Option<String>,
+    url : Option<String>,
+    carrerSuffix: Option<String>,
+    }
+
+
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
 }
